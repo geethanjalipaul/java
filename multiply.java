92a0c763164159de8897910e9dff80e903c27c8c
geethanjalipaul <geethanjali2212@gmail.com>
2022-04-06 11:39:04 +0530

    06.04

---
+++ multiply.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class multiply
+{
+        public static void main(String args[])
+        {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int num1=input.nextInt();
+        System.out.println("Enter the second number:");
+        int num2=input.nextInt();
+        int product=num1*num2;
+        System.out.println();
+        System.out.println(num1+"*"+num2+"="+product);
+        }
+}
--- product.java
+++ product.java
@@ -1,16 +1,29 @@
-
 import java.util.*;
 public class product
 {
-        public static void main(String args[])
-        {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        int num1=input.nextInt();
-        System.out.println("Enter the second number:");
-        int num2=input.nextInt();
-        int product=num1*num2;
-        System.out.println();
-        System.out.println(num1+"*"+num2+"="+product);
-        }
+static Product(String pname,String pcode,int price)
+{
+this.pname=pname ;
+this.pcode=pcode ;
+this. price=price;
+}
+
+public static void main(String args[])
+{
+Product p1=new Product("Benz","Car123",100000);
+System.out.println("*****Displaying product1*****") ;
+p1.display();
+
+Product p2 = new Product("Jaguar","Car426",250000);
+System.out.println("*****Displaying product2*****");
+p2.display();
+
+Product p3 = new Product("Maruthi","Car800",500000);
+System.out.println("*****Displaying product3*****");
+p3.display();
+
+Product p=p3.getPrice()<(p1.price<p2.price? p1.price:p2.price)?p3:(p1.price<p2.price?p1:p2);
+System.out.println("\n******Displaying product with the lowest price******");
+p.diplay();
+}
 }
