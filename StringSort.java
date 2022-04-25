888a7f61332fdb0524458319ef319a323f398ac2
geethanjalipaul <geethanjali2212@gmail.com>
2022-04-25 10:41:58 +0530

    25.04

---
+++ StringSort.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class StringSort
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number of strings:");
+int max=Integer.parseInt(sc.nextLine());
+String[]str=new String[max];
+System.out.println("Enter the numbers:");
+for(int i=0;i<max;i++)
+{
+System.out.print(i+":");
+str[i]=sc.nextLine();
+}
+System.out.println("Before sort:");
+System.out.println(Arrays.toString(str)+"\t");
+Arrays.sort(str);
+System.out.println("After sort:");
+System.out.print(Arrays.toString(str)+"\t");
+}
+}
---
+++ manipulation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class manipulation
+{
+public static void main(String args[])
+{
+Scanner obj=new Scanner(System.in);
+System.out.print("enter the first string:");
+String str=obj.nextLine();
+System.out.print("enter the second string:");
+String str2=obj.nextLine();
+System.out.println("Concatenation of string");
+String str3=str.concat(str2);
+System.out.println(str3);
+System.out.println("Length of the first string: "+str.length());
+System.out.println("character at position 4 in 1st string: "+str.charAt(2));
+System.out.println("Index of character 'd' in  2nd string: "+str2.indexOf('d'));
+}
+}
