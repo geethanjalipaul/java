8abc73f234f382c6e3678fa89aac0f32fc66d610
geethanjalipaul <geethanjali2212@gmail.com>
2022-05-26 23:31:18 +0530

    26.05

--- org/shapes/square.java
+++ org/shapes/square.java
@@ -1,13 +1,13 @@
-package org.shape; 
-public class square 
-{ 
-private int side; 
-public Square(int s) 
-{ 
-side=s; 
-} 
-public int area() 
-{ 
-return(side*side); 
-} 
-} 
+package org.shapes;
+public class square
+{
+	private int side;
+	public square(int s)
+	{
+		side=s;
+	}
+	public int area()
+	{
+		return(side*side);
+	}
+}
